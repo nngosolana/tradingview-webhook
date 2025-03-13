6e6215f5a9f0b92ccac9d76e5409dd95a9a25f73
nngostuds <nguyen.sudopixel@gmail.com>
2025-03-14 06:57:10 +0700

    upgrade library

--- lambda_package/config.py
+++ lambda_package/config.py
@@ -8,3 +8,4 @@ RISK_REWARD_RATIO = float(os.getenv("RISK_REWARD_RATIO", "2.0"))  # Default 2:1
 MACD_DELTA_RATIO = float(os.getenv("MACD_DELTA_RATIO", "0.66"))  # Default 0.66
 ENABLE_MACD_CHECK = os.getenv("ENABLE_MACD_CHECK", "True").lower() == "true"
 ORDER_MAX_LIMIT_PERCENTAGE = int(os.getenv("ORDER_MAX_LIMIT_PERCENTAGE", "20"))  #  max 20% of total balance per order
+DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL", "https://discord.com/api/webhooks/1341766666534518794/VUK5EA1L4JojCJ2HXSBkqQyf4T5YmE9pGxj2hw7YNETwNbBTWZQhTDTLL0WeMYLxnqCC")
